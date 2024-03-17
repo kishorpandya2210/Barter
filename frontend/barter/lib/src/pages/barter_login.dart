81f9a643419af9a2a19c2a41ff9c500d5a56044a
Kishor Pandya <kishorpandya17@gmail.com>
2024-03-17 16:59:55 -0400

    Added post request to check if the frontend hits backend endpoint

--- frontend/barter/lib/src/pages/barter_login.dart
+++ frontend/barter/lib/src/pages/barter_login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BarterLogin extends StatefulWidget {
   @override
@@ -9,9 +11,29 @@ class _BarterLoginState extends State<BarterLogin> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login() {
-    print(
-        'Username: ${_usernameController.text}, Password: ${_passwordController.text}');
+  Future<void> _login() async {
+    final String loginUrl = 'http://192.168.56.1:8080/login';
+    final response = await http.post(
+      Uri.parse(loginUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': _usernameController.text,
+        'password': _passwordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, then parse the JSON.
+      print('Login successful');
+      // Here, you can navigate to another screen or perform other actions.
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      print('Failed to log in: ${response.body}');
+      // Here, you can show an error message or perform other error handling.
+    }
   }
 
   @override
